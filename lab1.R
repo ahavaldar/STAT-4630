9ec2db84e28f128f7dff85d3bcf309c3067fcab7
ahavaldar <ash2sfp@virginia.edu>
2022-08-25 18:19:27 -0400

    lab 1

--- lab1.R
+++ lab1.R
@@ -30,7 +30,9 @@ plot(Data$sugars,Data$calories,xlab="Sugars per serving",	ylab="Calories per ser
 cor(Data$sugars, Data$calories)
 
 ##scatterplot matrix
-pairs(Data[,3:10], lower.panel = NULL, main="Scatterplot of Quantitative Variables")
+pairs(Data[,3:10], 
+      lower.panel = NULL, 
+      main="Scatterplot of Quantitative Variables")
 
 ##correlation matrix
 cor(Data[,3:10])
@@ -47,7 +49,7 @@ mytab<-table(Data$shelf, Data$mfr)
 mytab
 
 ##2 way table with proportions
-prop.table(mytab, 2)
+prop.table(mytab, 2)  # 2 means columns add up to 1
 
 ##############
 ##Regression##
@@ -59,24 +61,37 @@ result<-lm(calories~sugars+carbo+protein+fat+sodium+fiber+potass, data=Data)
 ##Diagnostics##
 ###############
 
-par(mfrow=c(2,2))
+par(mfrow=c(2,2))        # create 2x2 matrix of graphs to show on one plot
 plot(result)
 
 library(MASS)
 par(mfrow=c(1,1))
-MASS::boxcox(result)
+MASS::boxcox(result)  #log likelihood, if lambda=0 then perform a log transformation
 
 #############
 ##Inference##
 #############
 
 summary(result)
+# T test: For potass- Ho: B7 = 0, Ha: B7 doesnt equal 0
+# can we drop X7 from the model, while leaving the other predictors in?
+# Yes since pval is large
+
+# F test: Ho: B1=B2=B3=...= 0, Ha: at least one B in Ho is non zero
+# Comparison between our model vs an intercept only model
+# Is at least one predictor useful to the response variable
+# since p-val is small, reject null
+
+#sigma^2 is residual standard error squared
 
 ##fit reduced model
 reduced<-lm(calories~sugars+carbo+protein+fat, data=Data)
 
 ##partial F test
 anova(reduced,result)
+# since partial F test pval is large we fail to reject the null
+# Ho: supports the reduced model (FTR null means use reduced model)
+
 
 ##CI for parameters
 confint(reduced,level = 0.95)
@@ -93,6 +108,7 @@ reduced$coefficients
 ##########################
 
 class(Data$mfr)
+Data$mfr<-factor(Data$mfr)
 
 ##change reference class
 Data$mfr<-relevel(Data$mfr, ref = "other")
@@ -107,4 +123,7 @@ library(multcomp)
 pairwise<-multcomp::glht(reduced2, linfct = mcp(mfr= "Tukey"))
 summary(pairwise)
 
+# Ha: Ug - Uo = 0 vs Ho: Ug-Uo dne 0
+# We FTR the null
+# G and 0 have no stat significant difference in cals while controlling for the other predictors
 
---
+++ lab1quiz.R
@@ -0,0 +1,86 @@
+library(ISLR2)
+data <- Auto
+
+# 1) 392 rows, 9 columns
+# 2) Yes we should remove the names of the cars. Cannot run
+# a regression with this many unique variables
+# 3) 9 variables. 7 quantitative, 1 categorical (origin). 
+#  Distinction might not be obvious for year, since it can be put into factor levels. 
+#4) 
+data$origin <- factor(data$origin)
+levels(data$origin) <- c("American", "European", "Japanese")
+# We are assuming they can be treated as numeric values without factor levels
+# 5)
+pairs(data[,1:7], 
+      lower.panel = NULL, 
+      main="Scatterplot of Quantitative Variables")
+#6) 
+cor(data[,1:7])
+
+#7) From the scatterplot, we can see that gas mileage and year
+# have a positive correlation (0.58). Meaning that cars are getting better
+# gas mileage through the years. With gas mileage and weight,
+# we see a strong negative correlation (-0.83) which makes sense. 
+# Heavier cars will require more gas, reducing their MPG.
+# Engine displacement and weight have a very strong positive
+# correlation (0.93), since heavier cars tend to have greater
+# cylinder volume. Displacement and horsepower also have a 
+# strong positive correlation (0.89). 
+
+#8) 
+boxplot(data$mpg~data$origin, main="MPG by Origin")
+# From the plots, we can see that Japanese cars have the 
+# greatest MPG, while American cars aren't so great.
+
+#9) 
+result<-lm(mpg~cylinders+displacement+horsepower
+           +weight+acceleration+year, data=data)
+par(mfrow=c(2,2))   
+plot(result)
+
+library(MASS)
+par(mfrow=c(1,1))
+MASS::boxcox(result)  
+
+# The QQ plot looks good. The residuals seem to have a slight
+# fanning out pattern, which is something we might want to look at. 
+# The log likelihood verifies this concern, and we could possible
+# try a log transformation or raising the response variable to -0.5. 
+
+# 10)
+# I would agree, since 0.5 is in the middle of the log-likelihood graph. 
+
+#11) 
+data['mpg2'] = data['mpg'] **-0.5
+result2<-lm(mpg2~cylinders+displacement+horsepower
+           +weight+acceleration+year, data=data)
+par(mfrow=c(2,2))   
+plot(result2)
+
+library(MASS)
+par(mfrow=c(1,1))
+MASS::boxcox(result2)  
+# The plots verify our assumptions as does the log likelihood graph. 
+summary(result2)
+# a) The large F statistic and corresponding low p-val tells us that
+# at least one predictor is useful to the response variable. 
+# b) I will consider dropping displacement and acceleration. 
+# c) 
+reduced <- lm(mpg2~cylinders+horsepower
+              +weight+year, data=data)
+anova(reduced,result2)
+# We fail to reject the null of the anova test, which means 
+# we will use the reduced model. 
+reduced2 <- lm(mpg2~cylinders+horsepower
+              +weight+year+origin, data=data)
+
+library(multcomp)
+pairwise<-multcomp::glht(reduced2, linfct = mcp(origin= "Tukey"))
+summary(pairwise)
+# Japanese and European origin have no stat significant difference 
+# in mpg while controlling for the other predictors
+# European and American origin does have stat significant difference
+# in mpg, and so does Japanese and American origin. 
+# Japanese and European cars don't differ in fuel efficiency.
+# American cars however do differ in fuel efficienct when compared to 
+# European and Japanese cars. 
